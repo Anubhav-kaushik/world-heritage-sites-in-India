1ab251c7704907370b34e136916e200677a84ea9
Anubhav <anubhavsharma313@gmail.com>
2022-04-30 14:21:50 +0530

    basic version

---
+++ js/action.js
@@ -0,0 +1,55 @@
+function createInfoCard(numbering, title, year, state, imageUrl) {
+    const infoCard = document.createElement('div');
+    infoCard.classList.add('site-info-card');
+    infoCard.id = `info-card-${numbering}`;
+    infoCard.style.backgroundImage = `url(images/${imageUrl})`;
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('site-image');
+    // imageContainer.style.backgroundImage = `url(images/${imageUrl})`;
+    infoCard.appendChild(imageContainer);
+
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('site-info');
+
+    const titleContainer = document.createElement('h2');
+    titleContainer.classList.add('site-name');
+    titleContainer.innerText = title;
+    infoContainer.appendChild(titleContainer);
+
+    const yearContainer = document.createElement('p');
+    yearContainer.classList.add('year-of-recognition');
+    yearContainer.innerHTML = `<strong>${year}</strong>`;
+    infoContainer.appendChild(yearContainer);
+
+    const stateContainer = document.createElement('p');
+    stateContainer.classList.add('state');
+    stateContainer.innerText = state;
+    infoContainer.appendChild(stateContainer);
+
+    infoCard.appendChild(infoContainer);
+
+    const numberingContainer = document.createElement('div');
+    numberingContainer.classList.add('numbering');
+    numberingContainer.innerText = numbering;
+    infoCard.appendChild(numberingContainer);
+
+    console.log(infoCard);
+    return infoCard;
+}
+
+function addCardToDom(infoCard, containerSelector) {
+    const siteContainer = document.querySelector(containerSelector);
+    siteContainer.appendChild(infoCard);
+}
+
+function createInfoCards(data, containerSelector) {
+    const container = document.querySelector(containerSelector);
+    container.innerHTML = '';
+    for (let i in data) {
+        const infoCard = createInfoCard(i, data[i].name, data[i].yor, data[i].state, data[i].image);
+        addCardToDom(infoCard, containerSelector);
+    }
+}
+
+createInfoCards(heritageData, '.main-container');
---
+++ js/data.js
@@ -0,0 +1,242 @@
+const heritageData = {
+    1: {
+        name: "Agra fort",
+        yor: "1983",
+        state: "Uttar Pradesh",
+        image: "agra-fort.jpg"
+    },
+    2: {
+        name: "Ajanta Caves",
+        yor: "1983",
+        state: "Maharashtra",
+        image: "ajanta-caves.jpg"
+    },
+    3: {
+        name: "Ellora caves",
+        yor: "1983",
+        state: "Maharashtra",
+        image: "ellora-caves.jpg"
+    },
+    4: {
+        name: "Taj Mahal",
+        yor: "1983",
+        state: "Uttar Pradesh",
+        image: "taj-mahal.jpg"
+    },
+    5: {
+        name: "Monuments at Mahabalipuram",
+        yor: "1984",
+        state: "Tamil Nadu",
+        image: "mahabalipuram.jpg"
+    },
+    6: {
+        name: "Sun temple",
+        yor: "1984",
+        state: "Odisha",
+        image: "sun-temple.jpg"
+    },
+    7: {
+        name: "Kaziranga National Park",
+        yor: "1985",
+        state: "Assam",
+        image: "kaziranga-national-park.jpg"
+    },
+    8: {
+        name: "Keoladeo National Park",
+        yor: "1985",
+        state: "Rajasthan",
+        image: "keoladeo-national-park.jpg"
+    },
+    9: {
+        name: "Manas Wildlife Sanctuary",
+        yor: "1985",
+        state: "Assam",
+        image: "manas-wildlife-sanctuary.jpg"
+    },
+    10: {
+        name: "Churches and Convents of Goa",
+        yor: "1986",
+        state: "Goa",
+        image: "churches-and-convents-of-goa.jpg"
+    },
+    11: {
+        name: "Fatehpur Sikri",
+        yor: "1986",
+        state: "Uttar Pradesh",
+        image: "fatehpur-sikri.jpg"
+    },
+    12: {
+        name: "Monuments at Hampi",
+        yor: "1986",
+        state: "Karnataka",
+        image: "hampi.jpg"
+    },
+    13: {
+        name: "Khajuraho, Group of Temples",
+        yor: "1986",
+        state: "Madhya Pradesh",
+        image: "khajuraho.jpg"
+    },
+    14: {
+        name: "Elephanta Caves",
+        yor: "1987",
+        state: "Maharashtra",
+        image: "elephanta-caves.jpg"
+    },
+    15: {
+        name: "Sunderbans National Park",
+        yor: "1987",
+        state: "West Bengal",
+        image: "sunderbans-national-park.jpg"
+    },
+    16: {
+        name: "Chola Temples",
+        yor: "1987 & 2004",
+        state: "Tamil Nadu",
+        image: "chola-temples.jpg"
+    },
+    17: {
+        name: "Monuments at Pattadakal",
+        yor: "1987",
+        state: "Karnataka",
+        image: "pattadakal.jpg"
+    },
+    18: {
+        name: "Monuments at Sanchi",
+        yor: "1989",
+        state: "Madhya Pradesh",
+        image: "sanchi.jpg"
+    },
+    19: {
+        name: "Humayun's Tomb",
+        yor: "1993",
+        state: "Delhi",
+        image: "humayun-tomb.jpg"
+    },
+    20: {
+        name: "Qutub Minar and its Monuments",
+        yor: "1993",
+        state: "Delhi",
+        image: "qutub-minar.jpg"
+    },
+    21: {
+        name: "Nanda Devi and Valley of Flowers",
+        yor: "1998 & 2005",
+        state: "Uttarakhand",
+        image: "nanda-devi.jpg"
+    },
+    22: {
+        name: "Mahabodhi Temple",
+        yor: "2002",
+        state: "Bihar",
+        image: "mahabodhi-temple.jpg"
+    },
+    23: {
+        name: "Rock Shelters of Bhimbetka",
+        yor: "2003",
+        state: "Madhya Pradesh",
+        image: "rock-shelters-of-bhimbetka.jpg"
+    },
+    24: {
+        name: "Champanerpavagarh Fort",
+        yor: "2004",
+        state: "Gujarat",
+        image: "champanerpavagarh-fort.jpg"
+    },
+    25: {
+        name: "Chhatrapati Shivaji Terminus",
+        yor: "2004",
+        state: "Maharashtra",
+        image: "chhatrapati-shivaji-terminus.jpg"
+    },
+    26: {
+        name: "Red Fort",
+        yor: "2007",
+        state: "Delhi",
+        image: "red-fort.jpg"
+    },
+    27: {
+        name: "Mountain Railways of India",
+        yor: "1999, 2005 & 2008",
+        state: "West Bengal, Tamil Nadu & Himachal Pradesh",
+        image: "mountain-railways-of-india.jpg"
+    },
+    28: {
+        name: "The Jantar Mantar",
+        yor: "2010",
+        state: "Rajasthan",
+        image: "the-jantar-mantar.jpg"
+    },
+    29: {
+        name: "Western Ghats of India",
+        yor: "2012",
+        state: "Karnataka, Tamil Nadu, Kerala & Maharashtra",
+        image: "western-ghats-of-india.jpg"
+    },
+    30: {
+        name: "Hill Forts of Rajasthan",
+        yor: "2013",
+        state: "Rajasthan",
+        image: "hill-forts-of-rajasthan.jpg"
+    },
+    31: {
+        name: "Rani ki Vav",
+        yor: "2014",
+        state: "Gujarat",
+        image: "rani-ki-vav.jpg"
+    },
+    32: {
+        name: "Great Himalayan National Park",
+        yor: "2014",
+        state: "Himachal Pradesh",
+        image: "great-himalayan-national-park.jpg"
+    },
+    33: {
+        name: "Nalanda Mahavihara",
+        yor: "2016",
+        state: "Bihar",
+        image: "nalanda-mahavihara.jpg"
+    },
+    34: {
+        name: "Le Corbusier Work, Chandigarh",
+        yor: "2016",
+        state: "Chandigarh",
+        image: "le-corbusier.jpg"
+    },
+    35: {
+        name: "Khangchendzonga National Park",
+        yor: "2016",
+        state: "Sikkim",
+        image: "khangchendzonga-national-park.jpg"
+    },
+    36: {
+        name: "Historic City of Ahmedabad",
+        yor: "2017",
+        state: "Gujarat",
+        image: "ahmedabad.jpg"
+    },
+    37: {
+        name: "Victorian and Eco Ensemble",
+        yor: "2018",
+        state: "Maharashtra",
+        image: "victorian-and-eco-ensemble.jpg"
+    },
+    38: {
+        name: "Jaipur City",
+        yor: "2019",
+        state: "Rajasthan",
+        image: "jaipur-city.jpg"
+    },
+    39: {
+        name: "Kakatiya Rudrehwara",
+        yor: "2021",
+        state: "Telangana",
+        image: "kakatiya-rudrehwara.jpg"
+    },
+    40: {
+        name: "Dholavira City",
+        yor: "2021",
+        state: "Gujarat",
+        image: "dholavira-city.jpg"
+    }
+}
